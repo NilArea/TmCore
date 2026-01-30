e9cf7dcc15ccef054f7eebcffb7fe2024c641512
LeeQianXi <qianxilee232@163.com>
2026-01-30 20:44:16 +0800

    initial commit

---
+++ src/main/java/net/nilarea/tmcore/TechnoMatrixCore.java
@@ -0,0 +1,13 @@
+package net.nilarea.tmcore;
+
+import net.neoforged.fml.common.Mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(TechnoMatrixCore.MOD_ID)
+public class TechnoMatrixCore {
+
+    public static final String MOD_ID = "tmcore";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+}
